808c02a9b779a8e6fbad188de4d4788a5e9ba122
Anastasiia <a.liashchevska@gmail.com>
2026-05-22 19:19:57 +0300

    Move game status logic from template to computed

--- client/src/components/GameStatus.vue
+++ client/src/components/GameStatus.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="game-status">
-    <h2 v-if="!waiting && !isOver && !opponentLeft" :class="[
-      !waiting && yourTurn ? 'status-your-turn' : 'status-opponents-turn '
-    ]">
-      It's {{ yourTurn ? "your" : "opponents's" }} turn.
-    </h2>
-    <h2 v-if="waiting" class="status-waiting">waiting for an opponent</h2>
-    <h2 v-if="isOver" :class="youWon ? 'status-you-won' : 'status-you-lost'">
-      Game over. You {{ youWon ? "won!" : "lost!" }}
-    </h2>
-    <h2 v-if="opponentLeft && !isOver" class="status-opponent-left">
-      Opponent has left the game.
-    </h2>
+    game status: {{ currentStatus }}
   </div>
 </template>
 
@@ -23,13 +12,23 @@ export default {
   },
   computed: {
     ...mapState([
-      "shipsPlaced",
-      "gameStarted",
       "yourTurn",
       "isOver",
       "youWon",
       "opponentLeft"
-    ])
+    ]),
+    currentStatus() {
+      if (this.waiting) {
+        return 'waiting for an opponent'
+      }
+      if (this.isOver) {
+        return `game over | you ${this.youWon ? 'won' : 'lost'}`
+      }
+      if (this.opponentLeft) {
+        return 'game over | opponent left'
+      }
+      return `${this.yourTurn ? 'your' : "opponent's"} turn`
+    },
   }
 };
 </script>
